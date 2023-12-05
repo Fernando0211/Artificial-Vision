cf0bc96038601d8207d8690d3dfafcfb390196ca
Fernando0211 <84408583+Fernando0211@users.noreply.github.com>
2023-12-04 21:41:05 -0600

    Update Object trackig.py

--- Object trackig.py
+++ Object trackig.py
@@ -1,38 +1,33 @@
-import cv2 as cv
+import cv2
 import numpy as np
 
-vid = cv.VideoCapture('Object tracking.mp4')
+video = cv2.VideoCapture('Move detection.mp4')
 
-if (vid.isOpened()== False):
-	print("Error opening video file")
+ret, frame1 = video.read()
 
-while(vid.isOpened()):
-	
-	ret, frame = vid.read()
-	if ret == True:
-		#Preprocessing
-		imgray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-		blur = cv.GaussianBlur(imgray, (31, 31), 0)
-		edges = cv.Canny(imgray, 100, 150)
+frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+frame1 = cv2.GaussianBlur(frame1, (31, 31), 0)
+while True:
+	ret, frame2 = video.read()
+	if ret == False: break
+	gray = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+	gray = cv2.GaussianBlur(gray, (31, 31), 0)
 
-		#Contours
-		contours, hierarchy = cv.findContours(edges, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
-	    # Rectangle depending in the area of the contour
+	dif = cv2.absdiff(gray, frame1)
+	_, th = cv2.threshold(dif, 40, 255, cv2.THRESH_BINARY)
+		
+	cnts, _ = cv2.findContours(th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+		
+	for c in cnts:
+		area = cv2.contourArea(c)
+		if area > 120:
+			x,y,w,h = cv2.boundingRect(c)
+			cv2.rectangle(frame2, (x,y), (x+w,y+h),(0,255,0),2)
 
-		for contour in contours:
-			area = cv.contourArea(contour)
-			if area > 100:
-				x, y, w, h = cv.boundingRect(contour)
-   
-				cv.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+	cv2.imshow('Frame',frame2)
 
-		cv.imshow('Frame', frame)
-
-		if cv.waitKey(30) & 0xFF == ord('x'):
-			break
-	else:
+	if cv2.waitKey(30) & 0xFF == ord ('q'):
 		break
 
-vid.release()
-cv.destroyAllWindows()
+video.release()
